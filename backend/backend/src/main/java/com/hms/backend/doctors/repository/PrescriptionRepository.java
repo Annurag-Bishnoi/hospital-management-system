f1e78614593b9a0fc7ea7c2de955c941c8f2ff81
Anucdacgit <anuragbishnoi.tech@gmail.com>
2026-06-25 08:07:27 +0530

    Removed unrequired code from doctor module

--- backend/backend/src/main/java/com/hms/backend/doctors/repository/PrescriptionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hms.backend.doctors.repository;
-
-import com.hms.backend.doctors.entity.Prescription;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PrescriptionRepository
-        extends JpaRepository<Prescription, Long> {
-}
